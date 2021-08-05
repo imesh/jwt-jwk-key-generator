5ac56dcac585eb6210bb18a80353fa4ec2a6da96
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2021-08-05 12:19:31 +1000

    Verify JWT

--- src/main/java/imesh/examples/jwt/Main.java
+++ src/main/java/imesh/examples/jwt/Main.java
@@ -1,58 +1,37 @@
 package imesh.examples.jwt;
 
-import com.nimbusds.jose.Algorithm;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.crypto.ECDSASigner;
-import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.KeyUse;
-import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Date;
 
 public class Main {
 
   public static void main(String[] args) {
     try {
-      // Generate an EC key pair
-      ECKey keyPair = new ECKeyGenerator(Curve.P_256)
-          .keyID("123456789")
-          .algorithm(new Algorithm("ES256"))
-          .keyUse(KeyUse.ENCRYPTION)
-          .generate();
+      // Generate EC key pair
+      ECKey keyPair = ECKeyManager.generateECKey("123456789", JWSAlgorithm.ES256, KeyUse.SIGNATURE);
       System.out.println("EC Key Pair: " + keyPair);
+      System.out.println("EC Public Key: " + keyPair.toPublicJWK());
 
-      ECKey ecPublicKey = keyPair.toPublicJWK();
-      System.out.println("EC Public Key: " + ecPublicKey);
-
-      // Create the EC signer
-      JWSSigner signer = new ECDSASigner(keyPair);
-
-      // Prepare JWT with claims set
-      LocalDate expirationLocalDate = LocalDate.now().plusMonths(1);
-      Date expirationDate = Date
-          .from(expirationLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-      JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-          .subject("jwk")
-          .issuer("https://jwk.example.com")
-          .expirationTime(expirationDate)
-          .build();
-
-      SignedJWT signedJWT = new SignedJWT(
-          new JWSHeader.Builder(JWSAlgorithm.ES256).keyID(keyPair.getKeyID()).build(),
-          claimsSet);
-
-      // Compute the EC signature
-      signedJWT.sign(signer);
-
-      // Serialize the JWS to compact form
-      String jwt = signedJWT.serialize();
+      // Generate JWT using EC key pair
+      String subject = "jwt";
+      String issuer = "https://jwk.example.com";
+      String jwt = ECKeyManager.generateJWT(keyPair, subject, issuer);
       System.out.println("JWT: " + jwt);
+
+      // Verify JWT and retrieve JWT claim set
+      JWTClaimsSet jwtClaimsSet = ECKeyManager
+          .verifyJWT(jwt, keyPair.toPublicJWK(), JWSAlgorithm.ES256, subject, issuer);
+
+      // Verify the JWT claims
+      assertEquals(subject, jwtClaimsSet.getSubject());
+      assertEquals(issuer, jwtClaimsSet.getIssuer());
+      assertTrue(new Date().before(jwtClaimsSet.getExpirationTime()));
     } catch (Exception e) {
       e.printStackTrace();
     }
